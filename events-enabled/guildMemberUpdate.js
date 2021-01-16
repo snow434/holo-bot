801af82b51365aed4e129a9149d96c4a0b0a0a8c
snow434 <snow434.github@gmail.com>
2021-01-16 16:58:20 +0100

    Fix guildMemberUpdate not working properly

--- events-enabled/guildMemberUpdate.js
+++ events-enabled/guildMemberUpdate.js
@@ -2,20 +2,21 @@ const { MessageEmbed } = require("discord.js");
 
 module.exports = (client, oldMember, newMember) => {
 
-    if(oldMember.user.nickname != newMember.user.nickname) return;
-    const logEntry = new MessageEmbed()
-        .setTitle("Event: User nickname change")
-        .setColor("Yellow")
-        .setThumbnail(oldMember.user.displayAvatarURL({ dynamic: true }))
-        .addField("Before:", `Nickname: ${oldMember.nickname} \nDiscord tag: @${oldMember.user.tag}`)
-        .addField("After:", `Nickname: ${newMember.nickname} \nDiscord tag: @${oldMember.user.tag}`)
-        .setTimestamp()
-        .setFooter("Holo-bot");
-
-    const logChannel = newMember.guild.channels.cache.find(channel => channel.name === client.config.auditChannel);
-    if (!logChannel) {
-        console.log(`Audit log channel not defined or not found.`);
-    } else {
-        logChannel.send(logEntry);
+    if (oldMember.nickname == null) return;
+    if (oldMember.nickname != newMember.nickname) {
+        const logEntry = new MessageEmbed()
+            .setTitle("Event: User nickname change")
+            .setColor("Yellow")
+            .setThumbnail(oldMember.user.displayAvatarURL({ dynamic: true }))
+            .addField("Before:", `Nickname: ${oldMember.nickname} \nDiscord tag: @${oldMember.user.tag}`)
+            .addField("After:", `Nickname: ${newMember.nickname} \nDiscord tag: @${oldMember.user.tag}`)
+            .setTimestamp()
+            .setFooter("Holo-bot");
+        const logChannel = newMember.guild.channels.cache.find(channel => channel.name === client.config.auditChannel);
+        if (!logChannel) {
+            console.log(`Audit log channel not defined or not found.`);
+        } else {
+            logChannel.send(logEntry);
+        }
     }
 };
