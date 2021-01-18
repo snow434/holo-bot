c001b01b8c10f125f963a7074f19f2acf9f2fe99
snow434 <snow434.github@gmail.com>
2021-01-18 18:54:55 +0100

    Display list of servers where the bot is currently online

--- events-enabled/ready.js
+++ events-enabled/ready.js
@@ -1,5 +1,6 @@
 module.exports = (client, ready) => {
     console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Currently online on: ${client.guilds.cache.array()}`)
     client.user.setActivity(`${client.config.botStatus}`,
                     { type: `${client.config.botStatusType}` });
 }
