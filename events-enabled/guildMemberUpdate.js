b2c04c5e504f0df68f707ba0753e78dc407d3519
snow434 <snow434.github@gmail.com>
2021-01-18 17:13:48 +0100

    Remove 'if (oldMember.nickname == null)' Update embed formatting

--- events-enabled/guildMemberUpdate.js
+++ events-enabled/guildMemberUpdate.js
@@ -2,14 +2,13 @@ const { MessageEmbed } = require("discord.js");
 
 module.exports = (client, oldMember, newMember) => {
 
-    if (oldMember.nickname == null) return;
     if (oldMember.nickname != newMember.nickname) {
         const logEntry = new MessageEmbed()
             .setTitle("Event: User nickname change")
             .setColor("Yellow")
             .setThumbnail(oldMember.user.displayAvatarURL({ dynamic: true }))
-            .addField("Before:", `Nickname: ${oldMember.nickname} \nDiscord tag: @${oldMember.user.tag}`)
-            .addField("After:", `Nickname: ${newMember.nickname} \nDiscord tag: @${oldMember.user.tag}`)
+            .addField("Before:", `Nickname: ${oldMember.nickname} - ${oldMember.user.tag}`)
+            .addField("After:", `Nickname: ${newMember.nickname} - ${newMember.user.tag}`)
             .setTimestamp()
             .setFooter("Holo-bot");
         const logChannel = newMember.guild.channels.cache.find(channel => channel.name === client.config.auditChannel);
