cf01ac53d7d2698d7f26742e5a28e853041082b8
snow434 <snow434.github@gmail.com>
2021-01-20 21:40:12 +0100

    Add ability to specify the kick/ban reason and PM the user

--- commands-enabled/ban.js
+++ commands-enabled/ban.js
@@ -4,17 +4,18 @@ module.exports = {
     description: 'Ban the specified user from the server.',
     async execute(client, message, args) {
         const member = message.mentions.members.first();
+        const reason = args.slice(1).join(' ');
         if (member != null) {
             if (member === message.member || !member.bannable) {
                 message.reply("You cannot ban this user.")
                     .then(message => message.delete({ timeout: 5000 }))
                     .catch(console.error);
-            }
-            else {
-                message.mentions.members.first().ban();
+            } else {
+                message.mentions.members.first().send(`You were banned from the server ${message.guild.name} Reason: ${reason}`);
+                message.mentions.members.first().ban({ reason: `${reason}`});
                 message.reply("Banned successfully.")
-                .then(message => message.delete({ timeout: 5000 }))
-                .catch(console.error);
+                    .then(message => message.delete({ timeout: 5000 }))
+                    .catch(console.error);
             }
         } else {
             message.reply("Usage: !ban @user.")
--- commands-enabled/kick.js
+++ commands-enabled/kick.js
@@ -4,17 +4,18 @@ module.exports = {
     description: 'Kicks the specified user from the server.',
     async execute(client, message, args) {
         const member = message.mentions.members.first();
+        const reason = args.slice(1).join(' ');
         if (member != null) {
             if (member === message.member || !member.kickable) {
                 message.reply("You cannot kick this user.")
                     .then(message => message.delete({ timeout: 5000 }))
                     .catch(console.error);
-            }
-            else {
-                message.mentions.members.first().kick();
+            } else {
+                message.mentions.members.first().send(`You were kicked from the server ${message.guild.name} Reason: ${reason}`);
+                message.mentions.members.first().kick({ reason: `${reason}`});
                 message.reply("Kicked successfully.")
-                .then(message => message.delete({ timeout: 5000 }))
-                .catch(console.error);
+                    .then(message => message.delete({ timeout: 5000 }))
+                    .catch(console.error);
             }
         } else {
             message.reply("Usage: !kick @user.")
