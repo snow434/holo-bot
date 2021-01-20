0cbe0ead7e2a43f023367cbfd28125141fd93ba2
snow434 <snow434.github@gmail.com>
2021-01-20 19:33:05 +0100

    Fix "if (oldMessage.content != newMessage.content)" being in the wrong place

--- events-enabled/messageUpdate.js
+++ events-enabled/messageUpdate.js
@@ -2,29 +2,26 @@ const { MessageEmbed } = require("discord.js");
 
 module.exports = (client, oldMessage, newMessage) => {
 
-    // Avoid error: "MessageEmbed field values may not be empty." 
     // Event messageUpdate gets triggered on every MessageEmbed post.
     // This prevents the function from running again and referring to no longer existing oldMessage.
     if (oldMessage.author.bot) return;
-
-    const logEntry = new MessageEmbed()
-        .setTitle("Event: Edited message")
-        .setColor("Yellow")
-        .setThumbnail(oldMessage.author.displayAvatarURL({ dynamic: true }))
-        .addField("Message author:", `${oldMessage.author}`)
-        .addField("Message content before:", `${oldMessage.content}`)
-        .addField("Message content after:", `${newMessage.content}`)
-        .addField("Edited in:", `${oldMessage.channel}`)
-        .addField("Edited at:", `${newMessage.createdAt}`)
-        .setFooter("Holo-bot");
-
-    const logChannel = oldMessage.guild.channels.cache.find(channel => channel.name === client.config.messageLogChannel);
-    if (!logChannel)
-        console.log(`Message log channel not defined or not found.`);
     if (oldMessage.content != newMessage.content) {
-        logChannel.send(logEntry);
-    } else {
-        return;
-    }
+        const logEntry = new MessageEmbed()
+            .setTitle("Event: Edited message")
+            .setColor("Yellow")
+            .setThumbnail(oldMessage.author.displayAvatarURL({ dynamic: true }))
+            .addField("Message author:", `${oldMessage.author}`)
+            .addField("Message content before:", `${oldMessage.content}`)
+            .addField("Message content after:", `${newMessage.content}`)
+            .addField("Edited in:", `${oldMessage.channel}`)
+            .addField("Edited at:", `${newMessage.createdAt}`)
+            .setFooter("Holo-bot");
 
+        const logChannel = oldMessage.guild.channels.cache.find(channel => channel.name === client.config.messageLogChannel);
+        if (!logChannel) {
+            console.log(`Message log channel not defined or not found.`);
+        } else {
+            logChannel.send(logEntry);
+        }
+    }
 };
